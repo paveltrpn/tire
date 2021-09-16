8e4200b75501d586549598283fd973070db24a27
pavel_trpn <paveltrpn@mail.ru>
2021-09-16 23:10:34 +0400

    019 replace imgui.font() to imgui.font_push()

--- 019_ogl_modern_render_py/main.py
+++ 019_ogl_modern_render_py/main.py
@@ -28,6 +28,11 @@ def initGlfwWindow():
     [ver_min, ver_max, ver] = glfw.get_version()
     g_glfwVersion = str(ver_min) + "." + str(ver_max) + "." + str(ver)
 
+    #  ВКЛ-ВЫКЛ вертикальную синхронизацию (VSYNC)
+	#  Лок на 60 фпс
+    glfw.swap_interval(glfw.TRUE)
+
+
 def registerGlfwCallbacks():
     def keyCallback(window, key, scancode, action, mods):
         if (key == glfw.KEY_ESCAPE): 
@@ -52,6 +57,7 @@ def setOglDefaults():
 def initImgui():
     global g_imguiImpl
     global g_imguiVersion
+    global g_imguiRobotoFont
 
     imgui.create_context()
 
@@ -61,11 +67,10 @@ def initImgui():
     imgui.style_colors_dark()
     
     io = imgui.get_io()
-    global roboto
-    roboto = io.fonts.add_font_from_file_ttf("assets/RobotoMono-Medium.ttf", 16)
-    imgui.font(roboto)
+    
+    g_imguiRobotoFont = io.fonts.add_font_from_file_ttf("assets/RobotoMono-Medium.ttf", 16)
+    imgui.font(g_imguiRobotoFont)
     g_imguiImpl.refresh_font_texture()
-
 
 def loop():
     while not glfw.window_should_close(g_window):
@@ -77,14 +82,19 @@ def loop():
         g_imguiImpl.process_inputs()
         imgui.new_frame()
 
-        with imgui.font(roboto):
-            imgui.begin("019_python_OpenGL")
-            imgui.text("OpenGL GL_RENDERER - " + g_glRenderStr)
-            imgui.text("OpenGL GL_VERSION - " + g_glVersionStr)
-            imgui.text("Glfw version - " + g_glfwVersion)
-            imgui.text("Imgui version - " + g_imguiVersion)
-            imgui.end()
-    
+        # with imgui.font(roboto):
+
+        imgui.push_font(g_imguiRobotoFont)
+        imgui.begin("019_python_OpenGL")
+        imgui.text("Frame time - {f_time:3.2f} ms/frame {fps:3.2f}".format(f_time=(1000.0 / imgui.get_io().framerate), fps=imgui.get_io().framerate)); 
+        imgui.text("OpenGL GL_RENDERER - " + g_glRenderStr)
+        imgui.text("OpenGL GL_VERSION - " + g_glVersionStr)
+        imgui.text("OpenGL GLSL_VERSION - " + g_glslVersionStr)
+        imgui.text("Glfw version - " + g_glfwVersion)
+        imgui.text("Imgui version - " + g_imguiVersion)
+        imgui.end()
+        imgui.pop_font()
+        
         imgui.end_frame()
         imgui.render()
         g_imguiImpl.render(imgui.get_draw_data())
