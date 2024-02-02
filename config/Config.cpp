107cc39cd29797f3fcdb54c95eb9b611bbf29383
Pavel Turapin <paveltrpn@mail.ru>
2024-02-02 10:46:18 +0400

    Config json class enchancements

--- config/Config.cpp
+++ config/Config.cpp
@@ -5,6 +5,7 @@ module;
 #include <iostream>
 #include <format>
 #include <print>
+#include <type_traits>
 
 #include "nlohmann/json.hpp"
 
@@ -12,30 +13,75 @@ export module config;
 
 namespace tire {
 
+export using json = nlohmann::json;
+
+template <typename T>
+concept ConfigParamType
+  = std::is_same_v<bool, std::remove_cv_t<T>> || std::is_same_v<int, std::remove_cv_t<T>>
+    || std::is_same_v<float, std::remove_cv_t<T>>
+    || std::is_same_v<std::string, std::remove_cv_t<T>>
+    || std::is_same_v<json, std::remove_cv_t<T>>;
+
 export struct Config {
         Config() = default;
 
+        explicit Config(const char* lines) {
+            try {
+                config_ = json::parse(lines);
+            } catch (const json::parse_error& e) {
+                std::print("config json parse error\n"
+                           "message:\t{}\n"
+                           "exception id:\t{}\n"
+                           "byte position of error:\t{}\n",
+                           e.what(),
+                           e.id,
+                           e.byte);
+            }
+        }
+
         // clangd: modernize-pass-by-value! WAT???
-        Config(const nlohmann::json& json) : json_{ json } {
+        Config(const json& config) : config_{ config } {
         }
 
         [[nodiscard]]
         std::string getString(std::string_view param) const {
-            return json_[param];
+            return config_[param];
         }
 
         [[nodiscard]]
         int getInt(std::string_view param) const {
-            return json_[param];
+            return config_[param];
         }
 
         [[nodiscard]]
         bool getBool(std::string_view param) const {
-            return json_[param];
+            return config_[param];
+        }
+
+        [[nodiscard]]
+        float getFloat(std::string_view param) const {
+            return config_[param];
+        }
+
+        [[nodiscard]]
+        json getJson(std::string_view param) const {
+            return config_[param];
+        }
+
+        template <ConfigParamType T>
+        [[nodiscard]]
+        T get(std::string_view param) const {
+            T rt;
+            try {
+                rt = config_[param];
+            } catch (const json::exception& e) {
+                std::print("config param error:\t{}\n", e.what());
+            }
+            return rt;
         }
 
     private:
-        nlohmann::json json_;
+        json config_;
 };
 
 }  // namespace tire
--- test/window/main.cpp
+++ test/window/main.cpp
@@ -39,10 +39,11 @@ int main(int argc, char** argv) {
 }
 )";
 
-    nlohmann::json config = nlohmann::json::parse(configJson);
-    auto screenConfig = tire::Config{ config["Screen"] };
+    tire::Config config{ configJson };
 
-    auto screenType = screenConfig.getString("screen_type");
+    auto screenConfig = tire::Config{ config.get<tire::json>("Screen") };
+
+    auto screenType = screenConfig.get<std::string>("screen_type");
 
     std::unique_ptr<tire::Screen> scrn;
     if (screenType == "GLFW")
@@ -57,9 +58,9 @@ int main(int argc, char** argv) {
     auto renderType = screenConfig.getString("render_type");
 
     if (renderType == "OpenGL")
-        scrn->initRender(tire::RenderType::OPENGL, config["OpenGL"]);
+        scrn->initRender(tire::RenderType::OPENGL, config.get<tire::json>("OpenGL"));
     else if (renderType == "Vulkan")
-        scrn->initRender(tire::RenderType::VULKAN, config["Vulkan"]);
+        scrn->initRender(tire::RenderType::VULKAN, config.get<tire::json>("Vulkan"));
     else
         throw std::runtime_error("unknown render type\n");
 
