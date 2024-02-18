31d115f7d01a5e14598a19a99cb090d04d9441bc
Pavel Turapin <paveltrpn@mail.ru>
2024-02-18 14:07:17 +0400

    fetch new submodule name toy_std

--- geometry/Polytope.cpp
+++ geometry/Polytope.cpp
@@ -1,15 +1,37 @@
 
 module;
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 export module geometry;
+
+import toy_std;
 
 namespace tire {
 
-export enum class POLYTOPE { BOX, SPHERE };
+export enum class PolytopeType {
+    TETRAHEDRON,
+    OCTAHEDRON,
+    DODECAHEDRON,
+    ICOSAHEDRON,
+    BOX,
+    SPHERE,
+    TOURUS,
+    TEAPOT
+};
 
 export struct Polytope {
-        Polytope(POLYTOPE type) {
+        Polytope(PolytopeType type) {
         }
+
+    private:
+        std::vector<toy::algebra::vector3f> verticies_;
+        std::vector<int> indexes_;
+        std::vector<toy::algebra::vector3f> normals_;
+
+        toy::algebra::vector3f baseColor_;
 };
 
 }  // namespace tire
