05cae4c5c0495d93546f3902d3693b049f3bbe0c
Pavel Turapin <paveltrpn@mail.ru>
2024-03-15 11:52:08 +0400

    take vector from toy_std

--- modules/algebra/Vector.h
+++ modules/algebra/Vector.h
@@ -3,216 +3,376 @@
 #define __algebravectorbase_h__
 
 #include <cstddef>
+#include <initializer_list>
+#include <cmath>
 #include <array>
 
 namespace tire {
 
-template <template <typename> class derived_, typename T>
-struct _vector2_base {
-        using self = _vector2_base<derived_, T>;
-        using derived = derived_<T>;
+static constexpr size_t __SZ2 = 2;
+static constexpr size_t __SZ3 = 3;
+static constexpr size_t __SZ4 = 4;
 
-        template <typename U>
-        auto operator+(derived_<U> rhs) -> derived_<decltype(T{} + U{})> {
-            return { data_[0] + rhs.data_[0], data_[1] + rhs.data_[1] };
-        }
-
-        template <typename U>
-        auto operator-(derived_<U> rhs) -> derived_<decltype(T{} - U{})> {
-            return { data_[0] - rhs.data_[0], data_[1] - rhs.data_[1] };
-        }
-
-        template <typename U>
-        auto operator*(derived_<U> rhs) -> derived_<decltype(T{} * U{})> {
-            return { data_[0] * rhs.data_[0], data_[1] * rhs.data_[1] };
-        }
-
-        template <typename U>
-        auto operator/(derived_<U> rhs) -> derived_<decltype(T{} / U{})> {
-            return { data_[0] / rhs.data_[0], data_[1] / rhs.data_[1] };
-        }
-
-        template <typename U>
-        derived_<T> &operator-=(derived_<U> rhs) {
-            data_[0] -= rhs.data_[0];
-            data_[1] -= rhs.data_[1];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        template <typename U>
-        derived_<T> &operator+=(derived_<U> rhs) {
-            data_[0] += rhs.data_[0];
-            data_[1] += rhs.data_[1];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        template <typename U>
-        derived_<T> &operator*=(derived_<U> rhs) {
-            data_[0] *= rhs.data_[0];
-            data_[1] *= rhs.data_[1];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        template <typename U>
-        derived_<T> &operator/=(derived_<U> rhs) {
-            data_[0] /= rhs.data_[0];
-            data_[1] /= rhs.data_[1];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        std::array<T, 2> data_;
+template <typename T>
+struct lenght_type {
+        using type = float;
 };
 
-template <template <typename> class derived_, typename T>
-struct _vector3_base {
-        using self = _vector3_base<derived_, T>;
-        using derived = derived_<T>;
-
-        template <typename U>
-        auto operator+(derived_<U> rhs) -> derived_<decltype(T{} + U{})> {
-            return { data_[0] + rhs.data_[0], data_[1] + rhs.data_[1], data_[2] + rhs.data_[2] };
-        }
-
-        template <typename U>
-        auto operator-(derived_<U> rhs) -> derived_<decltype(T{} - U{})> {
-            return { data_[0] - rhs.data_[0], data_[1] - rhs.data_[1], data_[2] + rhs.data_[2] };
-        }
-
-        template <typename U>
-        auto operator*(derived_<U> rhs) -> derived_<decltype(T{} * U{})> {
-            return { data_[0] * rhs.data_[0], data_[1] * rhs.data_[1], data_[2] * rhs.data_[2] };
-        }
-
-        template <typename U>
-        auto operator/(derived_<U> rhs) -> derived_<decltype(T{} / U{})> {
-            return { data_[0] / rhs.data_[0], data_[1] / rhs.data_[1], data_[2] + rhs.data_[2] };
-        }
-
-        template <typename U>
-        derived_<T> &operator-=(derived_<U> rhs) {
-            data_[0] -= rhs.data_[0];
-            data_[1] -= rhs.data_[1];
-            data_[2] -= rhs.data_[2];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        template <typename U>
-        derived_<T> &operator+=(derived_<U> rhs) {
-            data_[0] += rhs.data_[0];
-            data_[1] += rhs.data_[1];
-            data_[2] += rhs.data_[2];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        template <typename U>
-        derived_<T> &operator*=(derived_<U> rhs) {
-            data_[0] *= rhs.data_[0];
-            data_[1] *= rhs.data_[1];
-            data_[2] *= rhs.data_[2];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        template <typename U>
-        derived_<T> &operator/=(derived_<U> rhs) {
-            data_[0] /= rhs.data_[0];
-            data_[1] /= rhs.data_[1];
-            data_[2] /= rhs.data_[2];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        std::array<T, 3> data_;
+template <>
+struct lenght_type<int> {
+        using type = float;
 };
 
-template <template <typename> class derived_, typename T>
-struct _vector4_base {
-        using self = _vector4_base<derived_, T>;
-        using derived = derived_<T>;
+template <>
+struct lenght_type<double> {
+        using type = double;
+};
+
+template <>
+struct lenght_type<long double> {
+        using type = long double;
+};
+
+template <template <typename> typename derived, typename T, size_t pSize_>
+struct vector_base {
+        using self = vector_base<derived, T, pSize_>;
+        using value_type = T;
+        using reference = T&;
+        using pointer = T*;
+
+        value_type& operator[](size_t id) {
+            return data_[id];
+        }
+
+        void sum(const self& b) {
+            for (size_t i = 0; i < pSize_; ++i)
+                data_[i] += b.data_[i];
+        };
+
+        void sub(const self& b) {
+            for (size_t i = 0; i < pSize_; ++i)
+                data_[i] -= b.data_[i];
+        };
 
         template <typename U>
-        auto operator+(derived_<U> rhs) -> derived_<decltype(T{} + U{})> {
-            return { data_[0] + rhs.data_[0], data_[1] + rhs.data_[1], data_[2] + rhs.data_[2], data_[3] + rhs.data_[3] };
+        void scale(U factor) {
+            for (size_t i = 0; i < pSize_; ++i)
+                data_[i] *= factor;
+        }
+
+        value_type dot(const self& b) const {
+            value_type rt{};
+
+            for (size_t i = 0; i < pSize_; ++i)
+                rt += data_[i] * b.data_[i];
+
+            return rt;
+        }
+
+        value_type sqLenght() const {
+            return dot(*this);
+        }
+
+        auto lenght() -> typename lenght_type<value_type>::type const {
+            return std::sqrt(sqLenght());
+        }
+
+        void noramlize() {
+            auto len = lenght();
+
+            // check len != 0
+
+            for (auto& elem : data_) {
+                elem /= len;
+            }
+        }
+
+        void inverse() {
+            for (auto& elem : data_) {
+                elem = -elem;
+            }
+        }
+
+        friend self operator+(const self& lhs, const self& rhs) {
+            auto rt = lhs;
+            rt.sum(rhs);
+            return rt;
+        }
+
+        self& operator+=(const self& rhs) {
+            (*this).sum(rhs);
+            return *this;
+        }
+
+        friend self operator-(const self& lhs, const self& rhs) {
+            auto rt = lhs;
+            rt.sub(rhs);
+            return rt;
+        }
+
+        self& operator-=(const self& rhs) {
+            (*this).sub(rhs);
+            return *this;
         }
 
         template <typename U>
-        auto operator-(derived_<U> rhs) -> derived_<decltype(T{} - U{})> {
-            return { data_[0] - rhs.data_[0], data_[1] - rhs.data_[1], data_[2] + rhs.data_[2], data_[3] - rhs.data_[3] };
+        friend self operator*(const self& lhs, U factor) {
+            auto rt = lhs;
+            rt.scale(factor);
+            return rt;
         }
 
         template <typename U>
-        auto operator*(derived_<U> rhs) -> derived_<decltype(T{} * U{})> {
-            return { data_[0] * rhs.data_[0], data_[1] * rhs.data_[1], data_[2] * rhs.data_[2], data_[3] * rhs.data_[3] };
+        self& operator*=(U factor) {
+            (*this).scale(factor);
+            return *this;
         }
 
-        template <typename U>
-        auto operator/(derived_<U> rhs) -> derived_<decltype(T{} / U{})> {
-            return { data_[0] / rhs.data_[0], data_[1] / rhs.data_[1], data_[2] + rhs.data_[2], data_[3] / rhs.data_[3] };
+        friend self operator*(const self& lhs, const self& rhs) {
+            auto rt = *lhs;
+            rt.dot(rhs);
+            return rt;
         }
 
-        template <typename U>
-        derived_<T> &operator-=(derived_<U> rhs) {
-            data_[0] -= rhs.data_[0];
-            data_[1] -= rhs.data_[1];
-            data_[2] -= rhs.data_[2];
-            data_[3] -= rhs.data_[3];
-            return static_cast<derived_<T> &>(*this);
+        self& operator*=(const self& rhs) {
+            (*this).dot(rhs);
+            return *this;
         }
 
-        template <typename U>
-        derived_<T> &operator+=(derived_<U> rhs) {
-            data_[0] += rhs.data_[0];
-            data_[1] += rhs.data_[1];
-            data_[2] += rhs.data_[2];
-            data_[3] += rhs.data_[3];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        template <typename U>
-        derived_<T> &operator*=(derived_<U> rhs) {
-            data_[0] *= rhs.data_[0];
-            data_[1] *= rhs.data_[1];
-            data_[2] *= rhs.data_[2];
-            data_[3] *= rhs.data_[3];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        template <typename U>
-        derived_<T> &operator/=(derived_<U> rhs) {
-            data_[0] /= rhs.data_[0];
-            data_[1] /= rhs.data_[1];
-            data_[2] /= rhs.data_[2];
-            data_[3] /= rhs.data_[3];
-            return static_cast<derived_<T> &>(*this);
-        }
-
-        std::array<T, 4> data_;
+        std::array<T, pSize_> data_;
 };
 
 template <typename T>
-struct vector2 : tire::_vector2_base<vector2, T> {
-        using tire::_vector2_base<vector2, T>::data_;
+struct vector2 : vector_base<vector2, T, __SZ2> {
+        using self = vector2<T>;
+        using value_type = T;
+        using reference = T&;
+        using pointer = T*;
 
-        T operator[](size_t id) {
-            return data_[id];
+        using vector_base<vector2, T, __SZ2>::data_;
+
+        [[nodiscard]]
+        value_type x() const {
+            return data_[0];
+        }
+
+        [[nodiscard]]
+        value_type y() const {
+            return data_[1];
+        }
+
+        friend self operator+(const self& lhs, const self& rhs) {
+            auto rt = lhs;
+            rt.sum(rhs);
+            return rt;
+        }
+
+        self& operator+=(const self& rhs) {
+            (*this).sum(rhs);
+            return *this;
+        }
+
+        friend self operator-(const self& lhs, const self& rhs) {
+            auto rt = lhs;
+            rt.sub(rhs);
+            return rt;
+        }
+
+        self& operator-=(const self& rhs) {
+            (*this).sub(rhs);
+            return *this;
+        }
+
+        template <typename U>
+        friend self operator*(const self& lhs, U factor) {
+            auto rt = lhs;
+            rt.scale(factor);
+            return rt;
+        }
+
+        template <typename U>
+        self& operator*=(U factor) {
+            (*this).scale(factor);
+            return *this;
+        }
+
+        friend self operator*(const self& lhs, const self& rhs) {
+            auto rt = *lhs;
+            rt.dot(rhs);
+            return rt;
+        }
+
+        self& operator*=(const self& rhs) {
+            (*this).dot(rhs);
+            return *this;
+        }
+};
+
+// vector3
+template <typename T>
+struct vector3 : vector_base<vector3, T, __SZ3> {
+        using self = vector3<T>;
+        using value_type = T;
+        using reference = T&;
+        using pointer = T*;
+
+        using vector_base<vector3, T, __SZ3>::data_;
+
+        [[nodiscard]]
+        value_type x() const {
+            return data_[0];
+        }
+
+        [[nodiscard]]
+        value_type y() const {
+            return data_[1];
+        }
+
+        [[nodiscard]]
+        value_type z() const {
+            return data_[2];
+        }
+
+        friend self operator+(const self& lhs, const self& rhs) {
+            auto rt = lhs;
+            rt.sum(rhs);
+            return rt;
+        }
+
+        self& operator+=(const self& rhs) {
+            (*this).sum(rhs);
+            return *this;
+        }
+
+        friend self operator-(const self& lhs, const self& rhs) {
+            auto rt = lhs;
+            rt.sub(rhs);
+            return rt;
+        }
+
+        self& operator-=(const self& rhs) {
+            (*this).sub(rhs);
+            return *this;
+        }
+
+        template <typename U>
+        friend self operator*(const self& lhs, U factor) {
+            auto rt = lhs;
+            rt.scale(factor);
+            return rt;
+        }
+
+        template <typename U>
+        self& operator*=(U factor) {
+            (*this).scale(factor);
+            return *this;
+        }
+
+        friend self operator*(const self& lhs, const self& rhs) {
+            auto rt = *lhs;
+            rt.dot(rhs);
+            return rt;
+        }
+
+        self& operator*=(const self& rhs) {
+            (*this).dot(rhs);
+            return *this;
+        }
+
+        void cross(self b) {
+            *this = { data_[1] * b[2] - data_[2] * b[1],
+                      data_[2] * b[0] - data_[0] * b[2],
+                      data_[0] * b[1] - data_[1] * b[0] };
+        }
+
+        self operator^(const self& rhs) {
+            auto rt = *this;
+            rt.cross(rhs);
+            return rt;
         }
 };
 
 template <typename T>
-struct vector3 : tire::_vector3_base<vector3, T> {
-        using tire::_vector3_base<vector3, T>::data_;
+struct vector4 : vector_base<vector4, T, __SZ4> {
+        using self = vector4<T>;
+        using value_type = T;
+        using reference = T&;
+        using pointer = T*;
 
-        T operator[](size_t id) {
-            return data_[id];
+        using vector_base<vector4, T, __SZ4>::data_;
+
+        [[nodiscard]] value_type x() const {
+            return data_[0];
+        }
+
+        [[nodiscard]] value_type y() const {
+            return data_[1];
+        }
+
+        [[nodiscard]] value_type z() const {
+            return data_[2];
+        }
+
+        [[nodiscard]] value_type w() const {
+            return data_[3];
+        }
+
+        friend self operator+(const self& lhs, const self& rhs) {
+            auto rt = lhs;
+            rt.sum(rhs);
+            return rt;
+        }
+
+        self& operator+=(const self& rhs) {
+            (*this).sum(rhs);
+            return *this;
+        }
+
+        friend self operator-(const self& lhs, const self& rhs) {
+            auto rt = lhs;
+            rt.sub(rhs);
+            return rt;
+        }
+
+        self& operator-=(const self& rhs) {
+            (*this).sub(rhs);
+            return *this;
+        }
+
+        template <typename U>
+        friend self operator*(const self& lhs, U factor) {
+            auto rt = lhs;
+            rt.scale(factor);
+            return rt;
+        }
+
+        template <typename U>
+        self& operator*=(U factor) {
+            (*this).scale(factor);
+            return *this;
+        }
+
+        friend self operator*(const self& lhs, const self& rhs) {
+            auto rt = *lhs;
+            rt.dot(rhs);
+            return rt;
+        }
+
+        self& operator*=(const self& rhs) {
+            (*this).dot(rhs);
+            return *this;
         }
 };
 
 template <typename T>
-struct vector4 : tire::_vector4_base<vector4, T> {
-        using tire::_vector4_base<vector4, T>::data_;
+struct dummy_derived {};
 
-        T operator[](size_t id) {
-            return data_[id];
-        }
-};
+template <typename T, size_t pSize_>
+using vector = vector_base<dummy_derived, T, pSize_>;
+
+using vector2i = vector2<int>;
+using vector3i = vector3<int>;
+using vector4i = vector4<int>;
 
 using vector2f = vector2<float>;
 using vector3f = vector3<float>;
@@ -221,10 +381,6 @@ using vector4f = vector4<float>;
 using vector2d = vector2<double>;
 using vector3d = vector3<double>;
 using vector4d = vector4<double>;
-
-using vector2i = vector2<long long>;
-using vector3i = vector3<long long>;
-using vector4i = vector4<long long>;
 
 }  // namespace tire
 
--- test/algebra/test.cpp
+++ test/algebra/test.cpp
@@ -14,18 +14,18 @@ namespace utf = boost::unit_test;
 
 BOOST_AUTO_TEST_SUITE(algebra)
 
-BOOST_AUTO_TEST_CASE(case_vector2_construct_and_arithmetic, *utf::tolerance(0.00001)) {
-    constexpr size_t __SZ = 2;
-    tire::vector2<float> one{ 0.0f, 1.0f };
-    tire::vector2<float> two{ 0.0f, 0.0f };
-    tire::vector2<float> three{ 1.0f, 1.0f };
+BOOST_AUTO_TEST_CASE(case_vector_construct_and_arithmetic, *utf::tolerance(0.00001)) {
+    constexpr size_t __SZ = 5;
+    tire::vector<float, __SZ> one{ 0.0f, 1.0f, 2.0f, 3.0f, 4.0f };
+    tire::vector<float, __SZ> two{ 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+    tire::vector<float, __SZ> three{ 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
 
     for (size_t i = 0; i < __SZ; ++i) {
         BOOST_TEST(two[i] == 0.0f);
         BOOST_TEST(three[i] == 1.0f);
     }
 
-    tire::vector2<float> four = three;
+    tire::vector<float, __SZ> four = three;
     four = two + three;
 
     for (size_t i = 0; i < __SZ; ++i) {
@@ -36,20 +36,20 @@ BOOST_AUTO_TEST_CASE(case_vector2_construct_and_arithmetic, *utf::tolerance(0.00
     // auto l = intv.lenght();
     // three = one + two;
 }
-/*
+
 BOOST_AUTO_TEST_CASE(case_vector2_construct_and_arithmetic, *utf::tolerance(0.00001)) {
-    toy::algebra::vector2f one{ 0.0f, 1.0f };
+    tire::vector2f one{ 0.0f, 1.0f };
 
     BOOST_TEST(one.x() == 0.0f);
     BOOST_TEST(one.y() == 1.0f);
 
-    toy::algebra::vector2f two{ 1.0f, 1.0f };
+    tire::vector2f two{ 1.0f, 1.0f };
     two.sum(one);
 
     BOOST_TEST(two.x() == 1.0f);
     BOOST_TEST(two.y() == 2.0f);
 
-    toy::algebra::vector2f three{ 0.0f, 0.0f };
+    tire::vector2f three{ 0.0f, 0.0f };
     three = one;
 
     BOOST_TEST(three.x() == one.x());
@@ -59,7 +59,7 @@ BOOST_AUTO_TEST_CASE(case_vector2_construct_and_arithmetic, *utf::tolerance(0.00
 
     BOOST_TEST(lenght == 5.0f);
 
-    toy::algebra::vector2f four = one + two + three;
+    tire::vector2f four = one + two + three;
 
     BOOST_TEST(four.x() == 1.0f);
     BOOST_TEST(four.y() == 4.0f);
@@ -68,20 +68,20 @@ BOOST_AUTO_TEST_CASE(case_vector2_construct_and_arithmetic, *utf::tolerance(0.00
 }
 
 BOOST_AUTO_TEST_CASE(case_vector3_construct_and_arithmetic, *utf::tolerance(0.00001)) {
-    toy::algebra::vector3f one{ 0.0f, 1.0f, 2.0f };
+    tire::vector3f one{ 0.0f, 1.0f, 2.0f };
 
     BOOST_TEST(one.x() == 0.0f);
     BOOST_TEST(one.y() == 1.0f);
     BOOST_TEST(one.z() == 2.0f);
 
-    toy::algebra::vector3f two{ 1.0f, 1.0f, 1.0f };
+    tire::vector3f two{ 1.0f, 1.0f, 1.0f };
     two.sum(one);
 
     BOOST_TEST(two.x() == 1.0f);
     BOOST_TEST(two.y() == 2.0f);
     BOOST_TEST(two.z() == 3.0f);
 
-    toy::algebra::vector3f three{ 0.0f, 0.0f, 0.0f };
+    tire::vector3f three{ 0.0f, 0.0f, 0.0f };
     three = one;
 
     BOOST_TEST(three.x() == one.x());
@@ -92,7 +92,7 @@ BOOST_AUTO_TEST_CASE(case_vector3_construct_and_arithmetic, *utf::tolerance(0.00
 
     BOOST_TEST(lenght == 5.0f);
 
-    toy::algebra::vector3f four = one + two + three;
+    tire::vector3f four = one + two + three;
 
     BOOST_TEST(four.x() == 1.0f);
     BOOST_TEST(four.y() == 4.0f);
@@ -100,5 +100,5 @@ BOOST_AUTO_TEST_CASE(case_vector3_construct_and_arithmetic, *utf::tolerance(0.00
 
     // auto cr = two.cross(one);
 }
-*/
+
 BOOST_AUTO_TEST_SUITE_END()
