83b227173942f6b0047e89db135ab60531d6f2d4
Pavel Turapin <paveltrpn@mail.ru>
2024-12-29 13:25:10 +0400

    format shader src

--- assets/shaders/vsSource-006.glsl
+++ assets/shaders/vsSource-006.glsl
@@ -11,18 +11,18 @@ uniform mat4 uProjectionMatrix;
 varying vec3 vLighting;
 varying vec4 vColor;
 
-void main(void) {
+void main( void ) {
     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
     vColor = aVertexColor;
 
-    vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-    vec3 directionalLightColor = vec3(1, 1, 1);
-    vec3 directionalVector = normalize(vec3(0, 0, 1));
+    vec3 ambientLight = vec3( 0.3, 0.3, 0.3 );
+    vec3 directionalLightColor = vec3( 1, 1, 1 );
+    vec3 directionalVector = normalize( vec3( 0, 0, 1 ) );
 
-    vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+    vec4 transformedNormal = uNormalMatrix * vec4( aVertexNormal, 1.0 );
 
-    float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
-    vLighting = ambientLight + (directionalLightColor * directional);
+    float directional =
+        max( dot( transformedNormal.xyz, directionalVector ), 0.0 );
+    vLighting = ambientLight + ( directionalLightColor * directional );
     //vLighting = vec3(0.5, 0.5, 0.5);
 }
-
