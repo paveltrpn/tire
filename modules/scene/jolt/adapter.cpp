4553e314d129bca06d4edd550998cccb7d6e2400
Pavel Turapin <paveltrpn@mail.ru>
2025-03-02 12:38:02 +0400

    update

--- modules/scene/jolt/adapter.cpp
+++ modules/scene/jolt/adapter.cpp
@@ -1,4 +1,23 @@
 
 #include "adapter.h"
 
-namespace tire::jolt {}
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Collision/Shape/SphereShape.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+
+namespace tire::jolt {
+
+JoltAdapter::JoltAdapter() {
+}
+
+JoltAdapter::~JoltAdapter() {
+}
+
+}  // namespace tire::jolt
--- modules/scene/jolt/adapter.h
+++ modules/scene/jolt/adapter.h
@@ -3,4 +3,21 @@
 
 #include "Jolt/Jolt.h"
 
-namespace tire::jolt {}
+#include <Jolt/Physics/PhysicsSystem.h>
+
+namespace tire::jolt {
+struct JoltAdapter final {
+    JoltAdapter();
+
+    JoltAdapter( const JoltAdapter& other ) = delete;
+    JoltAdapter( JoltAdapter&& other ) = delete;
+
+    JoltAdapter& operator=( JoltAdapter&& other ) = delete;
+    JoltAdapter& operator=( const JoltAdapter& other ) = delete;
+
+    ~JoltAdapter();
+
+private:
+    JPH::PhysicsSystem system_{};
+};
+}  // namespace tire::jolt
