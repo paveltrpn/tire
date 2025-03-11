01c8c58b2a26dc6e73db44d8adf2376059a61291
Pavel Turapin <paveltrpn@mail.ru>
2025-03-11 21:11:28 +0400

    update

--- modules/scene/flycam.h
+++ modules/scene/flycam.h
@@ -10,6 +10,11 @@ namespace tire {
 struct Flycam final {
 private:
     algebra::vector3f position_{};
+    algebra::vector3f look_{};
+    algebra::vector3f left_{};
+    // Default up vector +Y
+    algebra::vector3f up_{ 0.0f, 1.0f, 0.0f };
+
     float yaw_{};
     float pitch_{};
     float roll_{};
