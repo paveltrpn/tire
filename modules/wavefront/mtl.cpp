15885c751f10d700e7f283d5482f525e8a59d8cf
Pavel Turapin <paveltrpn@mail.ru>
2025-07-13 20:37:41 +0400

    start Mtl

--- modules/wavefront/mtl.cpp
+++ modules/wavefront/mtl.cpp
@@ -1,5 +1,25 @@
 module;
 
+#include <string>
+#include <format>
+
 export module wavefront:mtl;
 
-namespace tire {}
+namespace tire {
+
+export struct Mtl final {
+    Mtl() = delete;
+
+    Mtl( const Mtl &rhs ) = delete;
+    Mtl( Mtl &&rhs ) = delete;
+    Mtl &operator=( const Mtl &rhs ) = delete;
+    Mtl &operator=( const Mtl &&rhs ) = delete;
+
+    Mtl( std::string_view path ) {}
+
+    ~Mtl() = default;
+
+private:
+};
+
+}  // namespace tire
