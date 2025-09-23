44bfbbb44b301a4967f381627c331006e442f8b6
Pavel Turapin <paveltrpn@integra-s.com>
2025-09-23 10:54:29 +0400

    remove assets

--- assets/shaders/gl_color_FRAGMENT.glsl
+++
@@ -1,6 +0,0 @@
-#version 450 core
-out vec4 FragColor;
-in vec3 outColor;
-void main() {
-    FragColor = vec4(outColor, 0.5);
-};
--- assets/shaders/gl_color_VERTEX.glsl
+++
@@ -1,9 +0,0 @@
-#version 450 core
-layout (location = 0) in vec3 pos;
-out vec3 outColor;
-uniform mat4 view_matrix;
-uniform vec3 color;
-void main() {
-   outColor = color;
-   gl_Position = view_matrix * vec4(pos, 1.0);
-};
