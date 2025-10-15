065724d75e809cfe98aa232046b07d9dcfc990a9
Pavel Turapin <paveltrpn@integra-s.com>
2025-10-15 12:56:49 +0400

    flip image

--- modules/render/main_loop.cpp
+++ modules/render/main_loop.cpp
@@ -29,18 +29,18 @@ void RenderVK::frame( VkCommandBuffer cb ) {
     // Take out work from pipeline creation.
     // NOTE: Define negative viewport size to use same projection matrix as
     // for OpenGL pipeline.
-    // const VkViewport viewport{ .x = 0.0f,
-    //    .y = static_cast<float>( height_ ),
-    //    .width = static_cast<float>( width_ ),
-    //    .height = -static_cast<float>( height_ ),
-    //    .minDepth = 0.0f,
-    //    .maxDepth = 1.0f };
     const VkViewport viewport{ .x = 0.0f,
-                               .y = 0.0f,
+                               .y = static_cast<float>( height ),
                                .width = static_cast<float>( width ),
-                               .height = static_cast<float>( height ),
+                               .height = -static_cast<float>( height ),
                                .minDepth = 0.0f,
                                .maxDepth = 1.0f };
+    // const VkViewport viewport{ .x = 0.0f,
+    //    .y = 0.0f,
+    //    .width = static_cast<float>( width ),
+    //    .height = static_cast<float>( height ),
+    //    .minDepth = 0.0f,
+    //                         .maxDepth = 1.0f };
     vkCmdSetViewport( cb, 0, 1, &viewport );
 
     const VkRect2D scissor{ { .x = 0, .y = 0 },
