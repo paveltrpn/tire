041083b0cd6aa93af1a54794ddfca5b7ecb48166
Pavel Turapin <paveltrpn@mail.ru>
2025-10-17 21:30:27 +0400

    update

--- apps/qt/context_qt.cpp
+++ apps/qt/context_qt.cpp
@@ -14,6 +14,7 @@ ContextQt::ContextQt( VkInstance instance, VkPhysicalDevice pDevice,
                       VkDevice device, VkSurfaceKHR surface, VkRenderPass rp,
                       uint32_t gqfi, uint32_t gqi )
     : graphicsQueueId_{ gqi } {
+    // Vulkan resources ackquired from Qt.
     instance_ = instance;
     physDevice_ = pDevice;
     device_ = device;
@@ -38,9 +39,6 @@ auto ContextQt::init() -> void {
     const auto height = configHandle->get<int>( "window_height" );
     setViewportSize( width, height );
 
-    std::cout << surfaceCapabilities_.currentExtent.height
-              << " ================\n";
-    // Graphic queue.
     vkGetDeviceQueue( device_, graphicsFamilyQueueId_, graphicsQueueId_,
                       &graphicsQueue_ );
 }
