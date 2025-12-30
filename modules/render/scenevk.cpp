6539d46a22edf8252a7afe3f4b3228b23d810db4
Pavel Turapin <paveltrpn@integra-s.com>
2025-12-30 10:06:37 +0400

    update

--- modules/render/scenevk.cpp
+++ modules/render/scenevk.cpp
@@ -41,6 +41,8 @@ export struct SceneVK final : tire::Scene {
     }
 
     void submit() override {
+        const auto nodeListSize = bodyList_.size();
+
         // Update data in vulkan "vertex" buffers, i.e. copy from CPU memory.
         for ( size_t i = 0; auto &buffer : vertBuffersList_ ) {
             buffer->populate( reinterpret_cast<const void *>( bodyList_[i]->verteciesData() ) );
@@ -75,8 +77,9 @@ export struct SceneVK final : tire::Scene {
           cb, pipeline_->layout(), VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof( algebra::matrix4d ), &view );
 
         // Record draw commands for all scene objects in command buffer
-        for ( size_t object = 0; object < vertBuffersList_.size(); ++object ) {
+        for ( size_t object = 0; object < bodyList_.size(); ++object ) {
             const auto c = bodyList_[object]->albedoColor().asVector4f();
+
             vkCmdPushConstants(
               cb, pipeline_->layout(), VK_SHADER_STAGE_VERTEX_BIT, sizeof( algebra::matrix4d ),
               sizeof( algebra::vector4f ), &c );
@@ -88,6 +91,7 @@ export struct SceneVK final : tire::Scene {
             // NOTE: see https://docs.vulkan.org/guide/latest/vertex_input_data_processing.html
             std::array<VkBuffer, 2> vertexBuffers = { vbo, nbo };
             std::array<VkDeviceSize, 2> offsets = { 0, 0 };
+
             vkCmdBindVertexBuffers( cb, 0, 2, vertexBuffers.data(), offsets.data() );
             vkCmdDraw( cb, vCount, 3, 0, 0 );
         }
