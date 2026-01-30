b060a55e7e3a48bc26206327dc973401e56f1743
Pavel Turapin <paveltrpn@mail.ru>
2026-01-30 09:39:04 +0400

    got first draw by hand

--- src/modules/box/box.cpp
+++ src/modules/box/box.cpp
@@ -15,10 +15,10 @@ Box::Box( vsg::dvec3 pos, vsg::dvec3 scale ) {
     auto indicies = vsg::uintArray::create( { 0, 1, 2, 0, 2, 3, 4, 7, 6, 4, 6, 5, 0, 4, 7, 0, 7, 3,
                                               1, 5, 6, 1, 6, 2, 0, 3, 7, 0, 7, 4, 1, 2, 6, 1, 6, 5 } );
 
-    auto vertexDraw = vsg::VertexDraw::create();
+    auto vertexDraw = vsg::VertexIndexDraw::create();
     vertexDraw->assignArrays( { verticies, indicies } );
-    // vertexDraw->assignIndices( );
-    vertexDraw->vertexCount = 36;  // Number of indices
+    vertexDraw->assignIndices( indicies );
+    // vertexDraw->vertexCount = 36;  // Number of indices
     vertexDraw->instanceCount = 1;
 
     this->addChild( vertexDraw );
