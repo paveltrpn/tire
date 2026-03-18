3da8128c8b0e91aa58e3b5525a55d5400cfd0909
Pavel Turapin <paveltrpn@mail.ru>
2026-03-18 13:22:37 +0400

    update

--- src/tired/tired.cpp
+++ src/tired/tired.cpp
@@ -48,26 +48,31 @@ auto Tired::serviceObjectsSubgraph() -> ServiceObjectsSubgraph* {
 }
 
 auto Tired::init( Window* window, uint32_t width, uint32_t height ) -> void {
-    // set up the camera
-    auto lookAt =
-        vsg::LookAt::create( vsg::dvec3( 0.0, -4.0, 1.5 ), vsg::dvec3{ 0.0, 0.0, 0.0 }, vsg::dvec3( 0.0, 0.0, 1.0 ) );
+    // Setup the camera.
+    {
+        auto lookAt = vsg::LookAt::create( vsg::dvec3( 0.0, -4.0, 1.5 ), vsg::dvec3{ 0.0, 0.0, 0.0 },
+                                           vsg::dvec3( 0.0, 0.0, 1.0 ) );
 
-    vsg::ref_ptr<vsg::ProjectionMatrix> perspective =
-        vsg::Perspective::create( 30.0, static_cast<double>( width ) / static_cast<double>( height ), 0.01, 500.0 );
+        vsg::ref_ptr<vsg::ProjectionMatrix> perspective =
+            vsg::Perspective::create( 30.0, static_cast<double>( width ) / static_cast<double>( height ), 0.01, 500.0 );
 
-    camera_ = vsg::Camera::create( perspective, lookAt, vsg::ViewportState::create( VkExtent2D{ width, height } ) );
+        camera_ = vsg::Camera::create( perspective, lookAt, vsg::ViewportState::create( VkExtent2D{ width, height } ) );
+    }
 
-    _manipulator = new Manipulator{ camera_, nullptr };
-    _manipulator->trackball()->addWindow( *window );
+    // Setup manipulator and event handler objects.
+    {
+        _manipulator = new Manipulator{ camera_, nullptr };
+        _manipulator->trackball()->addWindow( *window );
+        _inputHandler = new InputHandler{ camera_ };
+    }
 
-    _inputHandler = new InputHandler{ camera_ };
-
-    viewer_->addEventHandler( _manipulator->trackball() );
-    viewer_->addEventHandler( _inputHandler->handler() );
-
-    auto commandGraph = vsg::createCommandGraphForView( *window, camera_, theRoot_ );
-
-    viewer_->addRecordAndSubmitTaskAndPresentation( { commandGraph } );
+    // Setup viewer object.
+    {
+        viewer_->addEventHandler( _manipulator->trackball() );
+        viewer_->addEventHandler( _inputHandler->handler() );
+        auto commandGraph = vsg::createCommandGraphForView( *window, camera_, theRoot_ );
+        viewer_->addRecordAndSubmitTaskAndPresentation( { commandGraph } );
+    }
 
     // Borrow vulkan data from VSG.
     {
@@ -81,38 +86,43 @@ auto Tired::init( Window* window, uint32_t width, uint32_t height ) -> void {
         context_ = std::make_shared<vk::Context>( instance, physicalDevice, device, surface, rp, 0, 0 );
     }
 
-    basemeshSubgraph_ = new BasemeshSubgraph{ this };
-    basemeshSubgraph_->initPipeline();
+    // Setup scenegraph.
+    {
+        basemeshSubgraph_ = new BasemeshSubgraph{ this };
+        basemeshSubgraph_->initPipeline();
 
-    obstaclesSubgraph_ = new ObstaclesSubgraph{ this };
-    obstaclesSubgraph_->initPipeline();
+        obstaclesSubgraph_ = new ObstaclesSubgraph{ this };
+        obstaclesSubgraph_->initPipeline();
 
-    serviceObjectsSubgraph_ = new ServiceObjectsSubgraph{ this };
-    serviceObjectsSubgraph_->initPipeline();
+        serviceObjectsSubgraph_ = new ServiceObjectsSubgraph{ this };
+        serviceObjectsSubgraph_->initPipeline();
 
-    connect( basemeshSubgraph_, &Subgraph::nodeAdded, this, [this]() {
-        viewer_->compileManager->compile( basemeshSubgraph_->stateGroup() );
-        vsg::CompileResult res{};
-        vsg::updateViewer( *viewer_, res );
-    } );
+        connect( basemeshSubgraph_, &Subgraph::nodeAdded, this, [this]() {
+            viewer_->compileManager->compile( basemeshSubgraph_->stateGroup() );
+            vsg::CompileResult res{};
+            vsg::updateViewer( *viewer_, res );
+        } );
 
-    theRoot_->addChild( basemeshSubgraph_->stateGroup() );
-    theRoot_->addChild( obstaclesSubgraph_->stateGroup() );
-    theRoot_->addChild( serviceObjectsSubgraph_->stateGroup() );
+        theRoot_->addChild( basemeshSubgraph_->stateGroup() );
+        theRoot_->addChild( obstaclesSubgraph_->stateGroup() );
+        theRoot_->addChild( serviceObjectsSubgraph_->stateGroup() );
 
-    auto box = vsg::ref_ptr<ExBox>(
-        new ExBox{ vsg::dvec3{ 0.0, 0.0, 0.0 }, vsg::dvec3{ 0.0, 0.0, 0.0 }, vsg::dvec3{ 1.0, 1.0, 1.0 } } );
-    basemeshSubgraph_->addChild( box );
+        auto box = vsg::ref_ptr<ExBox>(
+            new ExBox{ vsg::dvec3{ 0.0, 0.0, 0.0 }, vsg::dvec3{ 0.0, 0.0, 0.0 }, vsg::dvec3{ 1.0, 1.0, 1.0 } } );
+        basemeshSubgraph_->addChild( box );
+    }
 
     // Viewer compile.
-    constexpr auto UPDATE_INTERVAL{ 8 };
-    viewer_->setInterval( UPDATE_INTERVAL );
+    {
+        constexpr auto UPDATE_INTERVAL{ 8 };
+        viewer_->setInterval( UPDATE_INTERVAL );
 
-    constexpr auto CONTINOUS_UPDATE{ true };
-    viewer_->continuousUpdate = CONTINOUS_UPDATE;
+        constexpr auto CONTINOUS_UPDATE{ true };
+        viewer_->continuousUpdate = CONTINOUS_UPDATE;
 
-    viewer_->addEventHandler( vsg::CloseHandler::create( viewer_ ) );
-    viewer_->compile();
+        viewer_->addEventHandler( vsg::CloseHandler::create( viewer_ ) );
+        viewer_->compile();
+    }
 }
 
 }  // namespace tired
