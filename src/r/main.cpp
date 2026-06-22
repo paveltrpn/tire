772e13942ad687e2aa30eb4077ed24e071b9cfd6
Pavel Turapin <paveltrpn@mail.ru>
2026-06-22 12:08:36 +0400

    update

--- src/r/main.cpp
+++ src/r/main.cpp
@@ -1,7 +1,17 @@
 
-#include <print>
+#include "log/log.h"
+#include "window.h"
 
-auto main(int argc, char **argv) -> int {
-    std::println("tire");
+import config;
+
+auto main( int argc, char **argv ) -> int {
+    try {
+        new tire::Config{ "apps/config.json" };
+        auto foo = BareWindow{};
+        foo.loop();
+    } catch ( const std::exception &e ) {
+        // tire::log::error()( "caught exception: {}", e.what() );
+        return 1;
+    }
     return 0;
 }
