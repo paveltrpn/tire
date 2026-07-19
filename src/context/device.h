1b0dd420a61c2a4113b5d4e516d9d1fabf671896
Pavel Turapin <paveltrpn@integra-s.com>
2026-07-19 18:58:50 +0400

    update

--- src/context/device.h
+++ src/context/device.h
@@ -3,4 +3,17 @@
 
 namespace tire {
 
+struct VKDvice final {
+    VKDvice( const VKDvice &other ) = delete;
+    VKDvice( VKDvice &&other ) = delete;
+
+    auto operator=( const VKDvice &other ) -> VKDvice & = delete;
+    auto operator=( VKDvice &&other ) -> VKDvice & = delete;
+
+    ~VKDvice() = default;
+
+private:
+
+};
+
 }
--- src/context/vkinstance.h
+++ src/context/vkinstance.h
@@ -4,6 +4,13 @@
 namespace tire {
 
 struct VKInstance final {
+    VKInstance( const VKInstance &other ) = delete;
+    VKInstance( VKInstance &&other ) = delete;
+
+    auto operator=( const VKInstance &other ) -> VKInstance & = delete;
+    auto operator=( VKInstance &&other ) -> VKInstance & = delete;
+
+    ~VKInstance() = default;
 private:
 
 };
