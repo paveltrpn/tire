d32e77c660ed4f7d86990458e8c93fd0256d9791
Pavel Turapin <paveltrpn@integra-s.com>
2026-09-01 11:01:19 +0400

    update

--- modules/program/bytecodesource.cpp
+++ modules/program/bytecodesource.cpp
@@ -22,8 +22,13 @@ public:
     BytecodeProgramSource( std::string programName )
         : ProgramSource{ programName } {};
 
+    auto sources() const -> const std::unordered_map<ShaderStageType, std::vector<uint8_t>>& {
+        //
+        return _sources;
+    }
+
 private:
-    std::unordered_map<ShaderStageType, std::vector<uint8_t>> _shaders{};
+    std::unordered_map<ShaderStageType, std::vector<uint8_t>> _sources{};
 };
 
 }  // namespace tire
